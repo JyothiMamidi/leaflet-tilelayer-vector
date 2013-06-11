39f3cb10f33fd416901ef329005eb90c81054db9
Norbert Renner <norbert.renner@gmx.de>
2013-06-11 08:56:26 +0200

    add TileQueue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // npm/browserify
 // no explicit exports, as classes add themselves to 
 // the global Leaflet package structure 
+require('./TileQueue.js');
 require('./TileLayer.GeoJSON.js');
 require('./TileLayer.Vector.Unclipped.js');
 require('./TileLayer.Ajax.Binary.js');
