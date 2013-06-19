298d2ab182f13aa177f85957d548baaedc0d2934
Norbert Renner <norbert.renner@gmx.de>
2013-06-19 16:32:27 +0200

    patch conditional + export

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ require('./TileQueue.js');
 require('./TileLayer.GeoJSON.js');
 require('./TileLayer.Vector.Unclipped.js');
 require('./TileLayer.Ajax.Binary.js');
+require('./Leaflet.label-patch.js');
