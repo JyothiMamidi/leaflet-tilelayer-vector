2a3dd4e66ae183c1729e72808d92c74c6c566971
Eric Brelsford <ebrelsford@gmail.com>
2013-12-07 15:50:31 -0500

    Make AMD compatible

--- src/AbstractWorker.js
+++ src/AbstractWorker.js
@@ -1,25 +1,43 @@
-L.AbstractWorker = L.Class.extend({
-    initialize: function () {
-    },
 
-    onAdd: function (map) {
-    },
+(function () {
 
-    onRemove: function (map) {
-    },
+    function defineAbstractWorker(L) {
+        L.AbstractWorker = L.Class.extend({
+            initialize: function () {
+            },
 
-    process: function(tile, callback) {
-        callback(tile);
-    },
-    
-    abort: function(tile) {
-    },
-    
-    clear: function() {
+            onAdd: function (map) {
+            },
+
+            onRemove: function (map) {
+            },
+
+            process: function(tile, callback) {
+                callback(tile);
+            },
+            
+            abort: function(tile) {
+            },
+            
+            clear: function() {
+            }
+        });
+
+        // dummy worker (= no worker) when used directly
+        L.noWorker = function () {
+            return new L.AbstractWorker();
+        };
     }
-});
 
-// dummy worker (= no worker) when used directly
-L.noWorker = function () {
-    return new L.AbstractWorker();
-};
+    if (typeof define === 'function' && define.amd) {
+        // Try to add to Leaflet using AMD
+        define(['leaflet'], function (L) {
+            defineAbstractWorker(L);
+        });
+    }
+    else {
+        // Else use the global L
+        defineAbstractWorker(L);
+    }
+
+})();
--- src/TileCache.js
+++ src/TileCache.js
@@ -1,92 +1,110 @@
 /**
  * Simple tile cache to keep tiles while zooming with overzoom
  */
-L.TileCache = function() {
-};
 
-L.TileCache.prototype = {
-    // cache key: tile (String: Object)
-    _cache: {},
+(function () {
 
-    // flag to determine switch between tile unloading (put) and loading (get) phase
-    _unloading: false,
+    function defineTileCache(L) {
+        L.TileCache = function() {
+        };
 
-    // flag to only cache tiles when zooming, not when moving
-    _zooming: false,
+        L.TileCache.prototype = {
+            // cache key: tile (String: Object)
+            _cache: {},
 
-    onAdd: function(map) {
-        this._map = map;
-        
-        map.on('zoomstart', this._onZoomStart, this);
-        map.on('zoomend', this._onZoomEnd, this);
-    },
+            // flag to determine switch between tile unloading (put) and loading (get) phase
+            _unloading: false,
 
-    onRemove: function(map) {
-        this._map = null;
+            // flag to only cache tiles when zooming, not when moving
+            _zooming: false,
 
-        map.off('zoomstart', this._onZoomStart, this);
-        map.off('zoomend', this._onZoomEnd, this);
-    },
+            onAdd: function(map) {
+                this._map = map;
+                
+                map.on('zoomstart', this._onZoomStart, this);
+                map.on('zoomend', this._onZoomEnd, this);
+            },
 
-    _onZoomStart: function(evt) {
-        this._zooming = true;
-    },
+            onRemove: function(map) {
+                this._map = null;
 
-    _onZoomEnd: function(evt) {
-        this._zooming = false;
-    },
+                map.off('zoomstart', this._onZoomStart, this);
+                map.off('zoomend', this._onZoomEnd, this);
+            },
 
-    get: function(key, urlZoom) {
-        var ckey = this._getCacheKey(key, urlZoom);
-        var tile = this._cache[ckey];
-        this._unloading = false;
-        //console.log('cache ' + (tile ? 'hit ' : 'miss') + ': ' + ckey);
-        return tile;
-    },
-    
-    put: function(tile) {
-        if (!this._zooming) return;
+            _onZoomStart: function(evt) {
+                this._zooming = true;
+            },
 
-        if (!this._unloading) {
-            // clear old entries before adding newly removed tiles after zoom or move
-            this.clear();
-            this._unloading = true;
-        }
+            _onZoomEnd: function(evt) {
+                this._zooming = false;
+            },
 
-        var ckey = this._getCacheKeyFromTile(tile);
-        if (!(ckey in this._cache)) {
-            // vector layer is recreated because of feature filter
-            delete tile.layer;
-            this._cache[ckey] = tile;
-            //console.log('cache put : ' + ckey + ' (' + Object.keys(this._cache).length + ')');
-        }
-    },
-    
-    clear: function() {
-        //console.log('cache clear');
-        this._cache = {};
-    },
+            get: function(key, urlZoom) {
+                var ckey = this._getCacheKey(key, urlZoom);
+                var tile = this._cache[ckey];
+                this._unloading = false;
+                //console.log('cache ' + (tile ? 'hit ' : 'miss') + ': ' + ckey);
+                return tile;
+            },
+            
+            put: function(tile) {
+                if (!this._zooming) return;
 
-    _getCacheKeyFromTile: function(tile) {
-        return this._getCacheKey(tile.key, tile.urlZoom);
-    },
+                if (!this._unloading) {
+                    // clear old entries before adding newly removed tiles after zoom or move
+                    this.clear();
+                    this._unloading = true;
+                }
 
-    _getCacheKey: function(key, urlZoom) {
-        return urlZoom + ':' + key
+                var ckey = this._getCacheKeyFromTile(tile);
+                if (!(ckey in this._cache)) {
+                    // vector layer is recreated because of feature filter
+                    delete tile.layer;
+                    this._cache[ckey] = tile;
+                    //console.log('cache put : ' + ckey + ' (' + Object.keys(this._cache).length + ')');
+                }
+            },
+            
+            clear: function() {
+                //console.log('cache clear');
+                this._cache = {};
+            },
+
+            _getCacheKeyFromTile: function(tile) {
+                return this._getCacheKey(tile.key, tile.urlZoom);
+            },
+
+            _getCacheKey: function(key, urlZoom) {
+                return urlZoom + ':' + key
+            }
+        };
+
+        L.tileCache = function() {
+            return new L.TileCache();
+        };
+
+        // dummy impl. to turn caching off
+        L.tileCacheNone = function() {
+            return {
+                onAdd: function(map) {},
+                onRemove: function(map) {},
+                get: function(key, urlZoom) {},
+                put: function(tile) {},
+                clear: function() {}
+            };
+        };
     }
-};
 
-L.tileCache = function() {
-    return new L.TileCache();
-};
+    if (typeof define === 'function' && define.amd) {
+        // Try to add to Leaflet using AMD
+        define(['leaflet'], function (L) {
+            defineTileCache(L);
+        });
+    }
+    else {
+        // Else use the global L
+        defineTileCache(L);
+    }
 
-// dummy impl. to turn caching off
-L.tileCacheNone = function() {
-    return {
-        onAdd: function(map) {},
-        onRemove: function(map) {},
-        get: function(key, urlZoom) {},
-        put: function(tile) {},
-        clear: function() {}
-    };
-};
+})();
