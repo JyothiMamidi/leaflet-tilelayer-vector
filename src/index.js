a020bbd588a9cd82f3990f0fb160e3e5f62a5b42
Eric Brelsford <ebrelsford@gmail.com>
2014-12-10 11:26:32 -0500

    Remove Ajax.Binary

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ require('./TileCache.js');
 require('./TileQueue.js');
 require('./TileLayer.GeoJSON.js');
 require('./TileLayer.Vector.Unclipped.js');
-require('./TileLayer.Ajax.Binary.js');
 require('./TileLayer.Div.js');
 require('./TileLayer.Progress.js');
 require('./TileLayer.Overzoom.js');
